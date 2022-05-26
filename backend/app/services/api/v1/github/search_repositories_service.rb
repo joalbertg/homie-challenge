9dc6c9cea45cfb9e3dd50c91590d222d0997fd69
Joalbert <joalbertgonzalez@gmail.com>
2022-05-26 17:08:06 -0500

    [ADD] 🚀 SearchRepositoriesService class of Github
    - add `SearchRepositoriesService` class of Github
    - add `SearchRepositoriesService` class of Github tests
    - add `VCR` records for Github users

---
+++ backend/app/services/api/v1/github/search_repositories_service.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Github
+      class SearchRepositoriesService < V1::ApplicationService
+        def initialize(params)
+          super()
+
+          @params = params
+          @connection = params[:connection]
+          @options = params[:options] || {}
+        end
+
+        def call
+          response(success: true, payload: search_repositories)
+        rescue
+          response(error: StandardError.new(self))
+        end
+
+        private
+
+        attr_reader :params, :connection, :options
+
+        URL = 'https://api.github.com/users/'
+
+        def search_repositories
+          response = connection.get("#{URL}#{params[:username]}/repos", options)
+          raise unless response.status.in?(200..299)
+
+          response.body
+        end
+      end
+    end
+  end
+end
---
+++ backend/spec/services/api/v1/github/search_repositories_service_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+# vcr: { record: :none }
+RSpec.describe(Api::V1::Github::SearchRepositoriesService, type: :service, vcr: { record: :none }) do
+  describe '#call' do
+    let(:options) { { per_page: 2, page: 1 } }
+    let(:connection) do
+      Api::V1::HttpClient.call(token: 'bb06832b63a8639e04020967d048940feff31658').payload
+    end
+
+    context 'when it is success' do
+      describe 'with valid username' do
+        subject(:repositories) { repositories_response.payload }
+        
+        let(:repositories_response) { described_class.call(connection:, username: 'octocat', options:) }
+
+        it 'has correct types' do
+          expect(repositories_response).to have_attributes(success?: true, payload: Array, error: nil)
+        end
+
+        it { expect(repositories.size).to eq(2) }
+
+        it 'has all the right attributes' do
+          expect(repositories.first).to include(
+            'id' => 132935648,
+            'name' => 'boysenberry-repo-1',
+            'full_name' => 'octocat/boysenberry-repo-1',
+            'html_url' => 'https://github.com/octocat/boysenberry-repo-1',
+            'fork' => true,
+            'forks_count' => 9
+          )
+        end
+      end
+    end
+
+    context 'when it fails' do
+      describe 'with invalid username' do
+        it 'has correct types' do
+          result = described_class.call(connection:, username: nil)
+          expect(result).to have_attributes(success?: false, payload: {}, error: StandardError)
+        end
+      end
+    end
+  end
+end
