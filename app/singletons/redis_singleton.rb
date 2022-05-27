089e8a5950ec69bfdf86f11fbde717712c2bf07d
Joalbert <joalbertgonzalez@gmail.com>
2022-05-27 18:37:31 -0500

    [ADD] 🥷  sidekiq and redis
    - add redis cache store
    - add `RedisSingleton` class
    - add `Sidekiq`

---
+++ app/singletons/redis_singleton.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+class RedisSingleton
+  include Singleton
+  attr_reader :redis
+
+  class << self
+    delegate :redis, to: :instance
+  end
+
+  private
+
+  def initialize
+    uri = URI.parse(ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'))
+    @redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+
+    Rails.logger.info("Ping: #{redis.ping}")
+  rescue
+    Rails.logger.error(error.message)
+    raise(error)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use(ActionDispatch::Cookies)
+Sidekiq::Web.use(Rails.application.config.session_store, Rails.application.config.session_options)
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'health_check', to: proc { [200, {}, ['success']] }
 
