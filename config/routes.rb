a135caec828e174b8dc809b63b668b45668f2345
Joalbert <joalbertgonzalez@gmail.com>
2022-05-27 14:33:30 -0500

    [ADD] ❤️‍🩹 health_check and CI/CD

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'health_check', to: proc { [200, {}, ['success']] }
+
   namespace :api do
     namespace :v1 do
       resources :users, only: :show do
